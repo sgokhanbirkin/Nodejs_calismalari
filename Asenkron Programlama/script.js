91948d74fc276ce61499a896f903250fe488fb2a
gokhanbirkin <gkandth@gmail.com>
2022-01-30 23:54:16 +0300

    main

---
+++ Asenkron Programlama/script.js
@@ -0,0 +1,18 @@
+const first = () => {
+    console.log('First');
+    second();
+}
+
+const second = () => {
+    setTimeout(() => {
+        console.log('Second');
+    }, 2000);
+    
+    third();
+}
+
+const third = () => {
+    console.log('Third');
+}
+
+first();
